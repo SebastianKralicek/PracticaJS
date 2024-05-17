e76b6044a9376230d9188182418cb1d2b71036f7
SebastianKralicek <skralicek@udesa.edu.ar>
2024-05-16 23:37:00 -0300

    actividadesHechas

--- Javascript/index.js
+++ Javascript/index.js
@@ -132,3 +132,95 @@ console.log(frasehulk);
 for (let i=0; i<5; i++) {
     console.log("Practicando con el bucle for");
 }
+
+"B"
+for (let i=0; i<=10; i++) {
+    console.log("La variable i tiene el valor " + i );
+}
+
+"C"
+for(let i=2; i<=20; i+=2){
+    console.log("Multiplos de 2 " + i);
+}
+
+for (let i=5; i<=50; i+=5){
+    console.log("Multiplos de 5 " + i);
+}
+
+"D"
+for(let i=100; i>=0; i--){
+    console.log(i);
+}
+
+/*PROBLEMA 2*/
+var base = 5;
+var resultados =[];
+
+for (let i=1; i<=10; i++){
+    var resultado = base * i;
+    resultados.push(resultado);
+}
+console.log(resultados);
+
+/*PROBLEMA 3*/
+var ganancias = [10, 15, 40, 34, 50, 70, 3, 7, 9 , 11];
+var Total = 0;
+
+for (let i=0; i < ganancias.length; i++){
+    Total += ganancias[i];
+}
+console.log(Total);
+
+/*PROBLEMA 4*/
+let healingIsDifficult = [
+    'Fear',
+    'Drink to Get Drunk',
+    'Taken for Granted',
+    'Blow It All Away',
+    'Get Me',
+    'Im Not Important to You',
+    'Sober and Unkissed',
+    'Healing Is Difficult',
+    'Judge Me',
+    'Little Man',
+    'Insidiously'
+           ]
+for (let i = 0; i < healingIsDifficult.length; i++) {
+console.log(healingIsDifficult[i]);
+        }    "LISTA SIN NUMEROS"
+
+for (let i = 0; i < healingIsDifficult.length; i++) {
+console.log((i+1)+ "" + healingIsDifficult[i]);
+}               "LISTA CON NUMEROS"
+
+/*PROBLEMA 5*/
+let got = [
+    {
+        nombre: "Jon",
+        apellido: "Snow",
+        edad: 23,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Daenerys",
+        apellido: "Targaryen",
+        edad: 19
+    },
+    {
+        nombre: "Arya",
+        apellido: "Stark",
+        edad: 12,
+        ciudad: "Winterfell"
+    },
+    {
+        nombre: "Tyrion",
+        apellido: "Lannister",
+        edad: 32,
+        ciudad: "Casterly Rock"
+    }
+]
+for (let i=0; i<got.length; i++){
+    let personaje = got[i];
+    console.log("Hola " + personaje.nombre + personaje.apellido + " criatura viajeral");
+    console.log("Soy " + personaje.nombre + personaje.apellido + " de la ciudad " + personaje.ciudad);
+}
